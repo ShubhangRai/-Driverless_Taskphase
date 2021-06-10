2269c28e6a6941c3b9416072564218ecd634bf47
ShubhangRai <85261426+ShubhangRai@users.noreply.github.com>
2021-06-10 19:27:13 +0530

    Add files via upload

---
+++ Python Task Q2 Shubhang.py
@@ -0,0 +1,20 @@
+#Python Task Q2 Shubhang
+list = [2,7,5,4,5,2,8,9,1,2]
+print ("Original: " + str(list))
+res = []
+for i in list:
+	if i not in res:
+		res.append(i)
+print ("\n")
+print ("New : " + str(res))
+
+def add_tuple(x):
+    sum=0
+    for y in x:
+      sum +=y
+
+    return sum
+print ("\n")
+
+sum_tuple = add_tuple(res)
+print("sum=", sum_tuple)
