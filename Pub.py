3ca67345341128334ee75563d442dbbe386c28b3
ShubhangRai <85261426+ShubhangRai@users.noreply.github.com>
2021-07-02 18:41:46 +0530

    Add files via upload

---
+++ Pub.py
@@ -0,0 +1,11 @@
+import rospy
+from Tutorials.msg
+import Age
+rospy.shubh_node('Pub')
+pub=rospy.Publisher('Ages',Age,queue_size=10)
+msg=Age()
+
+while not rospy.is_shutdown():
+     msg.age1=input("Input 1st Age:")
+     msg.age2=input("Input 2nd Age:")
+     pub.publish(msg)
